185aeb65d75fa42485b1b0aca965c3ba69a7dcde
GettingThingsDone <123356140+Akinpete@users.noreply.github.com>
2023-11-30 05:44:23 +0100

    Delete 0x01-python-if_else_loops_functions directory

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-for firstN in range(10):
-    for secN in range(firstN + 1, 10):
-        if firstN == 8 and secN == 9:
-            print("{}{}".format(firstN, secN))
-        else:
-            print("{}{}, ".format(firstN, secN), end="")
