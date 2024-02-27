c2a1b73d662cf1a5f93f915e7c7c22a5d69bad9f
Akinpete <peter.akinpelumi@lifesci.uniben.edu>
2024-02-27 03:10:50 +0100

    maintains code

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -21,10 +21,10 @@ class Rectangle(Base):
             ValueError: If either of x or y < 0.
         """
         super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
 
     @property
     def width(self):
